611c526200ca9a457062cddc027bb74670805ec3
SravandeepReddy2004 <sravanreddy254@gmail.com>
2025-11-14 17:34:40 +0530

    initial commit

---
+++ src/main/java/com/myapp/demo/config/SchedulingConfig.java
@@ -0,0 +1,12 @@
+package com.myapp.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+/**
+ * Configuration class to enable scheduling
+ */
+@Configuration
+@EnableScheduling
+public class SchedulingConfig {
+}
