7806a091a8e63112990e06f980f3dde6b82dfd93
SravandeepReddy2004 <sravanreddy254@gmail.com>
2025-11-14 17:35:43 +0530

    initial commit

---
+++ src/main/java/com/myapp/demo/exception/ErrorResponse.java
@@ -0,0 +1,23 @@
+package com.myapp.demo.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+/**
+ * Standardized error response DTO
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ErrorResponse {
+    private LocalDateTime timestamp;
+    private int status;
+    private String error;
+    private String message;
+    private String path;
+}
